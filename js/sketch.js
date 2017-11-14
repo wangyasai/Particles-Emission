1506d6c049d78e9c12593fbd468b0d5d370d58b3
Yasai <wangys@thepaper.cn>
2017-11-14 23:30:59 +0800

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,47 @@
 var shadow;
 var theata = 0;
+var myCanvas;
+var img;
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    myCanvas = createCanvas(windowWidth, windowHeight);
+    background(20,20,20);
+
+}
+
+
+function preload(){
+    // if(gui.Image_DataURL){
+    //     img = loadImage(gui.Image_DataURL);
+    // }
+}
+
+function p5LoadImage(dataURL){
+    img = loadImage(dataURL);
 }
 
 function draw() {
+    drawParticles();
+}
 
+
+function drawParticles(){
+// shadow='rgba(20,20,20,'+(1-options.Shadow)+')';
 shadow='rgba(20,20,20,'+(1-options.Shadow)+')';
+// shadow ='rgba('+options.Background[0]+','
+//                +options.Background[1]+','
+//                +options.Background[2]+','
+//                +(1-options.Shadow)+')';
 
-background(shadow);
+background(options.Background);
+
+if(options.isPNG == true){
+    clear();
+}
 
 translate(width / 2, height / 2);
+
 
 for (var k = 0; k < options.Repeate; k++) {
         for (var i = 0; i < options.Points; i++) {
@@ -26,11 +56,11 @@ for (var k = 0; k < options.Repeate; k++) {
                 var r = map(dist(i * b, j * b, 0, 0), 0, scale,options.maxSize, options.minSize);
 
 
-            if(options.FullScreen == false){
-                if (dist(i * b, j * b, 0, 0) > scale) {
-                    r = 0;
+                if(options.FullScreen == false){
+                    if (dist(i * b, j * b, 0, 0) > scale) {
+                        r = 0;
+                    }
                 }
-            }
 
                 var percent = norm(pow(j + i, 1.2), 0, options.Points);
 
@@ -38,38 +68,59 @@ for (var k = 0; k < options.Repeate; k++) {
                 to = color(options.Color2);
                 between = lerpColor(from, to, percent);
        
-
+        
                 var x = i * b * abs((cos(angle))/2 );
                 var y = j * b * abs((cos(angle))/2 );
 
                 push();
                 rotate(radians(theata));
-                if(options.Shape  == 'Circle'){
+
+                if(type == 'image'){
                     if(options.Repeate == 1){
                         options.Repeate +=3;
-                    };
+                    }
+                    image(img,x,y);
+                }
+                else if(type =='text'){
+                    textSize(r);
+                    fill(between);
+                    options.Repeate = 1;
+                    var s = options.Text;
+                    text(s,x,y);
+                    text(s,-x,y);
+                    text(s,-x,-y);
+                    text(s,x,-y);
+                }
+                else if( type == 'Circle' ){                
+                    if(options.Repeate == 1){
+                        options.Repeate +=3;
+                    }
+
                     fill(between);
                     noStroke();
                     ellipse(x, y, r, r);  
-                  }else if(options.Shape == 'Rect'){
+                }
+                else if( type == 'Rectangle'){
                     if(options.Repeate == 1){
                         options.Repeate +=3;
-                    };
-                     fill(between);
-                     noStroke();
-                     rect(x, y, r, r); 
-                  }else if(options.Shape == 'Line'){        
-                   if(options.Repeate == 1){
+                    }
+                    print('rect');
+                    fill(between);
+                    noStroke();
+                    rect(x, y, r, r); 
+                }
+                else if(options.Shape == 'Line'){
+                    if(options.Repeate == 1){
                         options.Repeate +=3;
-                    };   
-                     stroke(between);
-                     strokeWeight(r/10);
-                     noFill();
-                     line(x, y, x+r*sin(angle), y+r*sin(angle)); 
-                  }else if (options.Shape == 'Diamond')  {
-                     if(options.Repeate == 1){
+                    }  
+                    stroke(between);
+                    strokeWeight(r/10);
+                    noFill();
+                    line(x, y, x+2*r*sin(angle), y+2*r*sin(angle)); 
+                }else if(options.Shape == 'Diamond')  {
+                    if(options.Repeate == 1){
                         options.Repeate +=3;
-                    };
+                    }
                      fill(between);
                      noStroke();
                      beginShape();
@@ -80,17 +131,15 @@ for (var k = 0; k < options.Repeate; k++) {
                      vertex(x+r,y-r);   
                      vertex(x+r/3*2,y-r/3); 
                      endShape(CLOSE);
-                  }else{
-                    textSize(r);
+                }else {
+                    if(options.Repeate == 1){
+                        options.Repeate +=3;
+                    }
                     fill(between);
-                    options.Repeate = 1;
-                    var s = options.Text;
-                    text(s,x,y);
-                    text(s,-x,y);
-                    text(s,-x,-y);
-                    text(s,x,-y);
-                  }
-                  pop();
+                    noStroke();
+                    ellipse(x, y, r, r);  
+                }
+              pop();
             }
         }
 
@@ -101,3 +150,8 @@ for (var k = 0; k < options.Repeate; k++) {
         rotate(TWO_PI / int(options.Repeate+0.1));
     }
 }
+
+
+
+
+
